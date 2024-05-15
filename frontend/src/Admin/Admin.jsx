1ed9b86aafcf2b70ec290b9484c959c18249d578
Ayannath12 <ayandebnath550@gmail.com>
2024-05-15 21:51:10 +0600

    add category option create

---
+++ frontend/src/Admin/Admin.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import SideMenu from "./SideMenu";
+import HomeInfo from "./HomeInfo";
+import { Outlet } from "react-router-dom";
+
+export default function Admin() {
+  return (
+    <div>
+      <Header />
+      <div className="container-fluid">
+        <div className="container-row">
+          <SideMenu />
+          <div className="py-10 box-right mb-12 col-md-9 col-lg-9 col-sm-12 lg:w-4/5 lg:px-3">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

8fed36fc49957177c9d8fa199e0f52688da8ab78
MD Fuad <fuadhasa199@gmail.com>
2024-05-16 10:27:53 +0600

    bug fixed & navbar menu add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import * as React from "react";
 import "./App.css";
 import NavBar from "./component/NavBar";
-import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import Footer from "./component/Footer";
 
 function App() {
   return (
-    <Box className="bg-rose-300 h-screen" sx={{ flexGrow: 1 }}>
+    <div className="bg-red-50 ">
       <NavBar />
       <Outlet />
-    </Box>
+      <Footer />
+    </div>
   );
 }
 
